27999fc2fd68c587598f6f761d01a44ade575023
Simon Hall <simon970@gmail.com>
2020-02-22 19:07:06 +1300

    task complete

--- classes.js
+++ classes.js
@@ -3,23 +3,36 @@
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
-function start () {
-  // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
-  // Your turn! Create a new function called `two`, then call it from here.
+function start() {
+    // The first example is done for you. Uncomment the line below and reload the browser.
+    one()
+    two()
+    three()
+    makeVisible()
+
+    // Your turn! Create a new function called `two`, then call it from here.
 }
 
-function one () {
-  // First, we have to find the element:
-  var one = document.getElementById('one')
+function one() {
+    // First, we have to find the element:
+    var one = document.getElementById('one')
 
-  // Next, we apply a new CSS class to it:
-  one.classList.add('blue')
+    // Next, we apply a new CSS class to it:
+    one.classList.add('blue')
 }
 
 // CREATE FUNCTION two HERE
-
+function two() {
+    var two = document.getElementById('two')
+    two.classList.add('green')
+}
 // CREATE FUNCTION three HERE
-
+function three() {
+    var three = document.getElementById('three')
+    three.classList.add('red')
+}
 // CREATE FUNCTION makeVisible HERE
+function makeVisible() {
+    var four = document.getElementsByClassName('invisible')[0]
+    four.classList.add('visible')
+}
